1df773b6d3f6f0ca9a51b745ad8afc407c3c3911
Roman Stratiienko <r.stratiienko@gmail.com>
2022-12-05 18:14:27 +0200

    patches: Build ffmpeg project with v4l2 request API support
    - Enable ffmpeg AOSPEXT configuration
    - Add FFmpeg patches from [JernejS][1] to support v4l2 request API
    
    [1]: https://github.com/jernejsk/FFmpeg
    Signed-off-by: Roman Stratiienko <r.stratiienko@gmail.com>

--- common/codecs/board.mk
+++ common/codecs/board.mk
@@ -3,3 +3,6 @@
 # GloDroid project (https://github.com/GloDroid)
 #
 # Copyright (C) 2022 Roman Stratiienko (r.stratiienko@gmail.com)
+
+BOARD_BUILD_AOSPEXT_FFMPEG := true
+BOARD_FFMPEG_SRC_DIR := glodroid/vendor/ffmpeg
